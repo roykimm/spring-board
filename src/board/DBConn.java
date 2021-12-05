04736f5897636cc0a317e2649c13f5625abecf29
kimmyunghoon <roykimmyunghoon@gmail.com>
2021-12-06 02:05:43 +0900

    change

--- src/board/DBConn.java
+++ src/board/DBConn.java
@@ -7,23 +7,23 @@ import java.sql.ResultSet;
 public class DBConn {
 
 	private Connection conn;
-		
+
 	public DBConn() {
-			
+
 		try {
-			String dbURL = "jdbc:mysql://gobella.kr:3306/bpj";
+			String dbURL = "";
 			String dbID = "bpj";
 			String dbPassword = "bpj";
 			Class.forName("com.mysql.jdbc.Driver");
 			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
-		
-		} catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public Connection getConn() {
 		return conn;
 	}
-	
+
 }
